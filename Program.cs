8de9837e7453a506f9e3a805fc64f9f5a3312ee2
Huseyn <your>
2020-04-14 13:31:54 +0300

    a class for SQL

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees emp=new Employees();
+            
+        }
+    }
+    class Employees
+    {
+        private int EmployeeID;
+        public string LastName;
+        public string FirstName;
+        public string Title;
+        public string TitleOfCourtesy;
+        public int BirthDate;
+        public DateTime Hire_date;
+
+        public string Address;
+        public Employees(int EmployeeID,string LastName, string FirstName, string Title, string 
+        TitleOfCourtesy,int BirthDate,DateTime Hire_date,string Address)
+        {
+            this.EmployeeID=EmployeeID;
+            this.LastName=LastName;
+            this.FirstName=FirstName;
+            this.Title=Title;
+            this.TitleOfCourtesy=TitleOfCourtesy;
+            this.BirthDate=BirthDate;
+            this.Hire_date=Hire_date;
+        }
+
+
+    }
+}
